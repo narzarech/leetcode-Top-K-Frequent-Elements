bed0836ff4e1c6f5b410745fa6adf0124a9268eb
narzarech <ntklxag@gmail.com>
2020-07-26 18:08:05 +0700

    working when the frequency of each number is different

--- Top K Frequent Elements/Source.cpp
+++ Top K Frequent Elements/Source.cpp
@@ -1,47 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <unordered_set>
+#include <map>
+#include <set>
 #include <algorithm>
 using namespace std;
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int>freq;
+        map<int, int>freq;
         int max = 1;
-        vector<int>sortFreq;
-        sortFreq.push_back(1);
+        vector<int>sorted;
+        sorted.push_back(1);
         for (auto num : nums) {
             ++freq[num];
-        }
-        for (auto elem : freq) {
-            sortFreq.push_back(elem.second);
-        }
-        sort(sortFreq.begin(), sortFreq.end());
-        unordered_set<int>rec;
-        for (int i = 0; i < k; ++i) {
-            rec.insert(sortFreq[sortFreq.size() - 1 - i]);
-        }
-        sortFreq.clear();
-        for (auto elem : freq) {
-            if (rec.find(elem.second) != rec.end()) {
-                sortFreq.push_back(elem.first);
+            if (freq[num] > max) {
+                max = freq[num];
+                sorted.push_back(max);
             }
         }
-        return sortFreq;
+        set<int>rec;
+        for (int i = sorted.size() - k; i < sorted.size(); ++i) {
+            rec.insert(sorted[i]);
+        }
+        sorted.clear();
+        for (auto elem : freq) {
+            if (rec.find(elem.second) != rec.end()) {
+                sorted.push_back(elem.first);
+            }
+        }
+        return sorted;
     }
 };
-int main() {
-    int tmp, k;
-    cin >> k;
-    vector<int>nums;
-    while (cin >> tmp) {
-        nums.push_back(tmp);
-    }
-    Solution sol;
-    nums = sol.topKFrequent(nums, k);
-    for (auto num : nums) {
-        cout << num << " ";
-    }
-}
 
